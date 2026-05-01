02ba1469a67baec678915445ccb02460d263cb36
Amir3356 <amirsiraj1995@gmail.com>
2026-05-01 20:37:47 +0300

    dggdgdg

--- backend/config/auth.php
+++ backend/config/auth.php
@@ -27,19 +27,19 @@ return [
     |
     | Next, you may define every authentication guard for your application.
     | Of course, a great default configuration has been defined for you
-    | which utilizes session storage plus the Eloquent user provider.
+    | which utilizes Eloquent user provider.
     |
     | All authentication guards have a user provider, which defines how the
     | users are actually retrieved out of your database or other storage
     | system used by the application. Typically, Eloquent is utilized.
     |
-    | Supported: "session"
+    | Supported: "token"
     |
     */
 
     'guards' => [
         'web' => [
-            'driver' => 'session',
+            'driver' => 'token',
             'provider' => 'users',
         ],
     ],
